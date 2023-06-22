65f6eff7a9802a81859d47edd36a7f8fad110694
Arman <arman18969@gmail.com>
2023-06-22 15:02:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+require("./db/conn");
+
+const cors = require("cors");
+const router = require("./Routes/router");
+const PORT = 6010;
+
+// app.get("/", (req, res) => {
+//   res.status(201).json("Server start");
+// });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static("./uploads"));
+app.use("/files", express.static("./public/files"));
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
