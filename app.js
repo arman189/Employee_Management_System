1d0e1a545de3d9742d1bfc34a5a5aa8812b49544
Arman <arman18969@gmail.com>
2023-06-22 15:14:15 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require("./db/conn");
 
 const cors = require("cors");
 const router = require("./Routes/router");
-const PORT = 6010;
+const PORT = process.env.PORT || 6010;
 
 // app.get("/", (req, res) => {
 //   res.status(201).json("Server start");
